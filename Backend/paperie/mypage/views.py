deccbf2f3d9bdeebd36839b8addf30e9cd942c16
isuHan <gkswltn3736@gmail.com>
2024-02-05 17:39:58 +0900

    2/5 mypage_save 수정

--- Backend/paperie/mypage/views.py
+++ Backend/paperie/mypage/views.py
@@ -4,17 +4,18 @@ from my_settings import DATABASES
 import mysql.connector
 import datetime
 import sys
-sys.path.append(r"C:/Users/김유진/OneDrive/문서/GitHub/SOLUX_Paperie/Backend/paperie/paperie")
+sys.path.append(r"C:\Users\한지수\Documents\GitHub\한지수\SOLUX_Paperie(4)\Backend\paperie")
 from django.db import connection
 import json
 
 def mypage_save(request):
     if request.method == 'POST':
-        # POST로 전달된 데이터 받기
-        title = request.POST.get('title')
-        content = request.POST.getlist('content')
-        type = request.POST.get('type')
-        ref = request.POST.get('ref')
+        body = request.body.decode('utf-8')
+        data = json.loads(body)
+
+        content = data.get('content')
+        ftype = data.get('type')
+        ref = data.get('ref')
 
         # 데이터베이스 연결 설정
         connection = mysql.connector.connect(
@@ -25,26 +26,17 @@ def mypage_save(request):
         )
         cursor = connection.cursor()
 
-        try:
-            # 쿼리와 데이터 준비
-            insert_query = "INSERT INTO result (title, content, ref, type, date) VALUES (%s, %s, %s, %s, %s)"
-            insert_data = (title, ', '.join(content), ref, type, datetime.datetime.now())
+        # 데이터 저장 쿼리 실행
+        insert_query = "INSERT INTO save (s_content, s_type, s_ref) VALUES (%s, %s, %s)"
+        values = (content, ftype, ref)  # 단일 값으로 수정
+        cursor.execute(insert_query, values)
+        connection.commit()
 
-            # 데이터베이스에 데이터 저장
-            with connection.cursor() as cursor:
-                cursor.execute(insert_query, insert_data)
-            connection.commit()
+        # 연결 종료
+        cursor.close()
+        connection.close()
 
-            # 저장 성공 메시지 반환
-            return HttpResponse("데이터 저장이 완료되었습니다.")
-
-        except Exception as e:
-            # 저장 실패 메시지 반환
-            return HttpResponse(f"데이터 저장 중 오류가 발생했습니다: {str(e)}")
-
-        finally:
-            # 연결 종료
-            connection.close()
+        return HttpResponse("데이터 저장이 완료되었습니다.")
 
     else:
         return HttpResponse("잘못된 요청입니다.")
