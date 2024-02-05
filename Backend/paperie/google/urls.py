85809a91ee51dee781d5121327bc2c8c0e1e303d
isuHan <gkswltn3736@gmail.com>
2024-02-06 03:27:09 +0900

    02/06 models.py 에러수정

--- Backend/paperie/google/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # 구글 소셜로그인
-    path('google/login', google_login, name='google_login'),
-    path('google/callback/', google_callback, name='google_callback'),
-    path('google/login/finish/', GoogleLogin.as_view(), name='google_login_todjango'),
-]
--- Backend/paperie/paperie/settings.py
+++ Backend/paperie/paperie/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'book',
     'news',
     'scholar',
-    'google',
 
     'rest_framework',
     'rest_framework.authtoken',
